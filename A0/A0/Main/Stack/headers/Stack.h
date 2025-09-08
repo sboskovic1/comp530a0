9c41e747a5e5505685aeb784c9e8608d5a6cf18d
Stefan <sb121@rice.edu>
2025-09-07 21:31:33 -0500

    done

--- A0/A0/Main/Stack/headers/Stack.h
+++ A0/A0/Main/Stack/headers/Stack.h
@@ -17,6 +17,23 @@ public:
 	/** WHATEVER CODE YOU NEED TO ADD HERE!! */
 	/*****************************************/
 
+    Data val() {
+        return holdMe;
+    }
+
+    Node (Data val) {
+        holdMe = val;
+        next = NULL;
+    }
+
+    Node *getNext() {
+        return next;
+    }
+
+    void setNext(Node *nextNode) {
+        next = nextNode;
+    }
+
 };
 
 // a simple LIFO stack
@@ -28,20 +45,41 @@ class Stack {
 public:
 
 	// destroys the stack
-	~Stack () { /* your code here */ }
+	~Stack () { 
+        Node<Data>* curr = head;
+        while (curr != NULL) {
+            Node<Data>* temp = curr;
+            curr = curr->getNext();
+            delete temp;
+        }
+    }
 
 	// creates an empty stack
-	Stack () { /* your code here */ }
+	Stack () { 
+        head = NULL;
+    }
 
 	// adds pushMe to the top of the stack
-	void push (Data) { /* your code here */ }
+	void push (Data pushMe) { 
+        Node<Data>* newNode = new Node<Data>(pushMe);
+        newNode->setNext(head);
+        head = newNode;
+    }
 
 	// return true if there are not any items in the stack
-	bool isEmpty () { /* replace with your code */ return true; }
+	bool isEmpty () { 
+        return head == NULL;
+    }
 
 	// pops the item on the top of the stack off, returning it...
 	// if the stack is empty, the behavior is undefined
-	Data pop () { /* replace with your code */ return Data ();  }
+	Data pop () { 
+        Node<Data> *temp = head;
+        head = head->getNext();
+        Data val = temp->val();
+        delete temp;
+        return val;
+    }
 };
 
 #endif
--- A0/A0/Main/StackTest/source/StackQUnit.cc
+++ A0/A0/Main/StackTest/source/StackQUnit.cc
@@ -6,6 +6,8 @@
 #include "Stack.h"
 #include <memory>
 #include <iostream>
+#include <string>
+#include <vector>
 
 int main () {
 
@@ -79,6 +81,50 @@ int main () {
 		
 		QUNIT_IS_EQUAL (myStack.isEmpty (), true);
 	}
+
+
+    {
+        Stack<std::string> stringStack;
+        
+        stringStack.push("Hello");
+        stringStack.push("World");
+        stringStack.push("Stack");
+        stringStack.push("Test");
+        
+        QUNIT_IS_EQUAL(stringStack.pop(), "Test");
+        QUNIT_IS_EQUAL(stringStack.pop(), "Stack");
+        QUNIT_IS_EQUAL(stringStack.pop(), "World");
+        QUNIT_IS_EQUAL(stringStack.pop(), "Hello");
+        
+        QUNIT_IS_EQUAL(stringStack.isEmpty(), true);
+    }
+
+    {
+        Stack<std::vector<int>> vectorStack;
+        
+        std::vector<int> vec1 = {1, 2, 3};
+        std::vector<int> vec2 = {4, 5, 6};
+        std::vector<int> vec3 = {7, 8, 9};
+        
+        vectorStack.push(vec1);
+        vectorStack.push(vec2);
+        vectorStack.push(vec3);
+        
+        // Verify vectors are popped in reverse order
+        std::vector<int> popped3 = vectorStack.pop();
+        QUNIT_IS_EQUAL(popped3.size(), 3);
+        QUNIT_IS_EQUAL(popped3[0], 7);
+        
+        std::vector<int> popped2 = vectorStack.pop();
+        QUNIT_IS_EQUAL(popped2.size(), 3);
+        QUNIT_IS_EQUAL(popped2[1], 5);
+        
+        std::vector<int> popped1 = vectorStack.pop();
+        QUNIT_IS_EQUAL(popped1.size(), 3);
+        QUNIT_IS_EQUAL(popped1[2], 3);
+        
+        QUNIT_IS_EQUAL(vectorStack.isEmpty(), true);
+    }
 }
 
 #endif
